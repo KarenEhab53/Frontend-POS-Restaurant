a4f634fe08e4a031440792144e38908bd9c09e19
esraa <esraaessam122002@gmail.com>
2026-04-28 18:18:28 +0300

    COMMIT

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@
 
 import Auth from './Pages/Auth/Auth';
 import './App.css'
-import Dashboard from "./Pages/Dashboard/Dashboard";
+
 
 
 function App() {
   return(
   <>
-    <Dashboard />
+  
        <Auth/>
   </>
   )
